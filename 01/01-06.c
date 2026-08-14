66e0ef5a541043a153f90403512b3f10e520e6c0
Andre Ivo <andreivo637@gmail.com>
2026-08-14 11:22:46 -0300

    exec 01-06

---
+++ 01/01-06.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    double hours, salary;
+    scanf("%d %lf %lf", &number, &hours, &salary);
+    double total_salary = hours * salary;
+    printf("NUMBER = %d\n", number);
+    printf("SALARY = U$ %.2lf\n", total_salary); 
+}
