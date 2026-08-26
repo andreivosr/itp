3767bb7da15af36eab805fcb612eadc6cfb9c549
Andre Ivo <andreivo637@gmail.com>
2026-08-26 17:09:13 -0300

    pratice c

---
+++ pratice/03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 25;
+    float altura = 1.75f; // numero decimal (precisão simples)
+    double peso = 68.555; // numero decimal (precisão dupla)
+    char letra = 'A'; // caractere
+    char nome[20] = "João"; // string (array de caracteres)
+
+
+
+    return 0;
+}
